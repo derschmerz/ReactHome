c88ae0a1fe07169e8d91bab995061b848086d1f7
derschmerz <derschmerz2@gmail.com>
2021-12-11 11:00:59 +0300

    сдфыыцщкл1

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
+// const element = <h1>Hello world</h1>
+
+// const elementTwo = React.createElement(
+//   "h1",
+//   { className: "test" },
+//   "Привет мир"
+// );
+
+// ReactDOM.render(elementTwo, document.getElementById("root"));
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
