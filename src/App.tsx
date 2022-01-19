1e75468390a996073cd0b8b197e926d55e9441e3
derschmerz <derschmerz2@gmail.com>
2022-01-19 17:14:27 +0300

    fix git

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+<<<<<<< HEAD
 import Header from "./Components/Header";
 import Login from "./Components/Login";
 import Footer from "./Components/Footer";
@@ -12,6 +13,12 @@ const App = () => {
       <Footer />
     </div>
   );
+=======
+import RegistrationPage from "./containers/RegistrationPage";
+
+const App = () => {
+  return <RegistrationPage />;
+>>>>>>> 1e4ce8ed855dffb68e0036bad59124252bfddbab
 };
 
 export default App;
