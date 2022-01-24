076cd180afd04a9dda5bcb5712596c8d3c369002
derschmerz <derschmerz2@gmail.com>
2022-01-24 21:05:16 +0300

    green

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
 import Header from "./Components/Header";
 import Login from "./Components/Login";
 import Footer from "./Components/Footer";
@@ -13,12 +12,6 @@ const App = () => {
       <Footer />
     </div>
   );
-=======
-import RegistrationPage from "./containers/RegistrationPage";
-
-const App = () => {
-  return <RegistrationPage />;
->>>>>>> 1e4ce8ed855dffb68e0036bad59124252bfddbab
 };
 
 export default App;
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,22 +1,21 @@
 import React from "react";
-import styled from "styled-components";
 
 const Header: React.FC = () => {
   return (
     <header>
       <div className="container">
         <a href="#0">
-          <img className="Logo" src="/images/icon2.svg" alt="" />
+          <img className="Logo" src="/images/video-game.png" alt="" />
         </a>
         <div className="Menu">
-          <a href="#1">Каталог</a>
-          <a href="#2">Где купить</a>
-          <a href="#3">Доставка и оплата</a>
-          <a href="#4">Контакты </a>
+          <a href="#1">Games</a>
+          <a href="#2">Магазин</a>
+          <a href="#3">Новости</a>
+          <a href="#4">Сообщество </a>
         </div>
         <div className="RightMenu">
-          <a href="#5">Корзина</a>
-          <a href="#6">Account</a>
+          <a href="#5">Поддержка</a>
+          <a href="#6">Моя учетная запись</a>
         </div>
       </div>
     </header>
