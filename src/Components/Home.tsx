d8eaff7bc6cff5fcc6157c6705d3d8b94686994c
Derschmerz <derschmerz2@gmail.com>
2022-01-28 12:30:53 +0300

    show all button

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -6,6 +6,7 @@ const Home = () => {
   return (
     <Container>
       <TopGames />
+      <AllGamesButton>SHOW ALL GAMES</AllGamesButton>
     </Container>
   );
 };
@@ -25,6 +26,28 @@ const Container = styled.div`
     right: 0;
     background-image: url("/images/backbody.jpg");
     z-index: -1;
+  }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const AllGamesButton = styled.a`
+  background-color: #0e443e;
+  font-weight: bold;
+  padding: 17px 9px;
+  color: #f9f9f9;
+  border-radius: 4px;
+  text-align: center;
+  font-size: 20px;
+  cursor: pointer;
+  transition: all 250ms;
+  letter-spacing: 1.5px;
+  margin: 30px 20px;
+  max-width: 700px;
+  &:hover {
+    background: #64d8cb;
   }
 `;
 
