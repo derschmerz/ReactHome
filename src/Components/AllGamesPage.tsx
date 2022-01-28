5277d4118dd8b8d5134b63915410b5b6bf452293
Derschmerz <derschmerz2@gmail.com>
2022-01-28 15:37:34 +0300

    allgames

---
+++ src/Components/AllGamesPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const AllGamesPage = () => {
+  return <Container>All Games</Container>;
+};
+
+export default AllGamesPage;
+
+const Container = styled.div``;
